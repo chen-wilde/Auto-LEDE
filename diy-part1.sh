3f95670fbbc447733d5488a95342340445bc1748
chen-wilde <151738787+chen-wilde@users.noreply.github.com>
2024-08-20 11:12:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/ s/^/#/' package/boot/uboot-rk35xx/Makefile
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
