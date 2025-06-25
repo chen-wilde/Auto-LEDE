746a9ab4f6600a20364ade505e359a3721411388
chen-wilde <151738787+chen-wilde@users.noreply.github.com>
2025-06-25 23:00:21 +0800

    Add cloudflared

--- remote.sh
+++ remote.sh
@@ -54,6 +54,7 @@ config tailscale 'settings'
 EOF
 
 echo "$FRPC_CONFIG" > files/etc/config/frpc
+echo "$TUNNEL_CONFIG" > files/etc/config/cloudflared
 echo "$ZEROTIER_CONFIG" > files/etc/config/zerotier
 
 sed -i "s/\\\$1\\\$[^:]*:0:/$LEDE_PASSWD/g" openwrt/package/lean/default-settings/files/zzz-default-settings
